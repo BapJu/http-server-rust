688202a1a68d800dcf7167905bbb7ec623d09b43
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2025-04-18 14:58:03 +0200

    get path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -11,18 +11,44 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     //
     for stream in listener.incoming() {
-         match stream {
-             Ok(mut _stream) => {
-                println!("accepted new connection");
-                //add response
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                _stream.write_all(response.as_bytes()).unwrap();
-                 
+        match stream {
+            Ok(mut stream) => { 
+                // Création d'un buffer pour stocker les données reçues
+                let mut buffer = [0; 1024];
+                
+                // Lecture des données depuis le stream
+                match stream.read(&mut buffer) {
+                    Ok(_) => {
+                        println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+                        //Get request path
+                        let request = String::from_utf8_lossy(&buffer);
+                        let request_lines: Vec<&str> = request.split("\r\n").collect();
+                        let request_line = request_lines[0];
+                        let request_parts: Vec<&str> = request_line.split_whitespace().collect();
+                        let path = request_parts[1];
+
+                        // Logique de traitement de la requête
+                        let mut response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                        if path == "/" {
+                            response = "HTTP/1.1 200 OK\r\n\r\n";
+                        }
+                        
+                        // Préparation et envoi de la réponse
+                        stream.write_all(response.as_bytes()).unwrap();
+
+                        
+                        // Assurez-vous que toutes les données sont envoyées avant de passer à la prochaine itération
+                        stream.flush().unwrap();
+                    },
+                    Err(e) => {
+                        println!("Erreur de lecture: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-    
 }
